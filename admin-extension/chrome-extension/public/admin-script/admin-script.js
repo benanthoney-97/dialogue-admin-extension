3a793febd0ba9c77cbaa357b0273e1aaa5e478b4
Ben Anthoney <ben@usedistil.com>
2026-01-26 19:58:46 +0000

    Removed non highlight

--- admin-extension/chrome-extension/public/admin-script/admin-script.js
+++ admin-extension/chrome-extension/public/admin-script/admin-script.js
@@ -94,7 +94,6 @@
     span.style.setProperty("transition", "border-color 0.2s ease, color 0.2s ease", "important");
     span.style.setProperty("line-height", "1.2", "important");
     span.style.setProperty("display", "inline-block", "important");
-    span.style.setProperty("background-color", "transparent", "important");
   };
 
   const setInactiveInlineStyles = (span) => {
