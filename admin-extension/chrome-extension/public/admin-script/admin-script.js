f8e11b6b4d698157a64740abb030574e5a79e52b
Ben Anthoney <ben@usedistil.com>
2026-01-28 19:44:29 +0000

    More shimmer

--- admin-extension/chrome-extension/public/admin-script/admin-script.js
+++ admin-extension/chrome-extension/public/admin-script/admin-script.js
@@ -89,7 +89,7 @@
     span.style.setProperty("font-weight", "400", "important");
     span.style.setProperty("transition", "border-color 0.2s ease, color 0.2s ease", "important");
     span.style.setProperty("line-height", "1.2", "important");
-    span.style.setProperty("display", "inline", "important");
+    span.style.setProperty("display", "inline-block", "important");
     span.style.setProperty("padding", "0 4px", "important");
   };
 
